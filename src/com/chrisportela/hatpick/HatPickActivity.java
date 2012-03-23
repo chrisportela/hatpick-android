9b6e28849e68bc90961739919131add8f49fc95a
Chris Portela <chris@chrisportela.com>
2012-03-23 11:25:53 -0400

    Project Made

---
+++ src/com/chrisportela/hatpick/HatPickActivity.java
@@ -0,0 +1,13 @@
+package com.chrisportela.hatpick;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HatPickActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
