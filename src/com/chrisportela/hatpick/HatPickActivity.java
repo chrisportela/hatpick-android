4582f13f292a721e8796cac1f615ceeee9d2bf70
Chris Portela <chris@chrisportela.com>
2012-03-23 15:24:48 -0400

    First working version
    Can add items to the list
    Items added to list can be removed using dialog to remove
    Items can be picked randomly by button at bottom and shown to a dialog
    If the user likes the choice then it exits, else it leaves the user to try again

--- src/com/chrisportela/hatpick/HatPickActivity.java
+++ src/com/chrisportela/hatpick/HatPickActivity.java
@@ -1,13 +1,119 @@
 package com.chrisportela.hatpick;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class HatPickActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	ArrayList<String> listItems = new ArrayList<String>();
+	Button addItemBtn;
+	Button pickItemBtn;
+	ListView itemList;
+	EditText itemEditText;
+	ArrayAdapter<String> listAdapter;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		addItemBtn = (Button) findViewById(R.id.addItemBtn);
+		pickItemBtn = (Button) findViewById(R.id.pickItemBtn);
+		itemList = (ListView) findViewById(R.id.itemList);
+		itemEditText = (EditText) findViewById(R.id.itemEditText);
+
+		listAdapter = new ArrayAdapter<String>(HatPickActivity.this,
+				android.R.layout.simple_list_item_1, listItems);
+		listAdapter.setNotifyOnChange(true);
+		itemList.setAdapter(listAdapter);
+
+		addItemBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				listItems.add(itemEditText.getText().toString());
+				listAdapter.notifyDataSetChanged();
+				/*
+				 * Toast.makeText(getApplicationContext(), "Added Item: " +
+				 * itemEditText.getText().toString(),
+				 * Toast.LENGTH_SHORT).show();
+				 */
+				// ensure the text box is cleared afterwards
+				itemEditText.setText("");
+				// call refresh of list
+			}
+		});
+
+		itemList.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// When clicked, show a toast with the TextView text
+				Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				final int pos = position;
+				AlertDialog.Builder builder = new AlertDialog.Builder(HatPickActivity.this);
+				builder.setMessage("Do you want to delete this item?")
+						.setCancelable(false)
+						.setPositiveButton("Yes",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog, int id) {
+										HatPickActivity.this.listItems.remove(pos);
+									}
+								})
+						.setNegativeButton("No", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
+				AlertDialog alert = builder.create();
+				alert.show();
+			}
+		});
+
+		pickItemBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				pickItem();
+			}
+		});
+
+	}
+
+	public void pickItem() {
+		int item = -1;
+		item = (int) ((double) Math.random() * (double) listItems.size());
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(listItems.get(item))
+				.setTitle("Picking out of the hat...")
+				.setCancelable(false)
+				.setPositiveButton("Alright",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								HatPickActivity.this.finish();
+							}
+						})
+				.setNegativeButton("No", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		AlertDialog alert = builder.create();
+		alert.show();
+		listAdapter.notifyDataSetChanged();
+	}
 }
---
+++ src/com/chrisportela/hatpick/PickItemActivity.java
@@ -0,0 +1,15 @@
+package com.chrisportela.hatpick;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PickItemActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pick_item_layout);
+	}
+
+}
